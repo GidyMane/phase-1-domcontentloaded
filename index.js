cd4eeed6f64888dcbaa5194aa5c417983742de0e
GidyMane <muragegideon2000@gmail.com>
2023-10-09 14:15:46 +0300

    Event Listener for Loading js file last

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", ()=>{
+    let p= document.querySelector("#text");
+    p.textContent ="This is really cool!"
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
